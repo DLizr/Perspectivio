33e29bb1ba95a121d05409a84c86186ba3beed49
Dlizr <Dmitryl@finfly.ru>
2019-12-14 08:32:22 +0300

    Added static wireframe

--- src/rendering/object/StaticCube.py
+++ src/rendering/object/StaticCube.py
@@ -1,4 +1,5 @@
 from src.rendering.object.StaticSceneObject import StaticSceneObject
+from src.rendering.object.Wireframe import Wireframe
 
 
 class StaticCube(StaticSceneObject):
@@ -7,6 +8,7 @@ class StaticCube(StaticSceneObject):
         self.__centerPos = centerPosition
         self.__width = width
         vertices = self.genVertices()
+        print(vertices)
         indices = [1, 3, 7, 5, 
                    3, 2, 6, 7, 
                    2, 6, 4, 0, 
@@ -16,6 +18,7 @@ class StaticCube(StaticSceneObject):
         if not colors:
             colors = [1.0] * 24
         super().__init__(vertices, indices, colors)
+        self.__genWireframe(vertices)
     
     def genVertices(self) -> list:
         vertices = []
@@ -27,3 +30,11 @@ class StaticCube(StaticSceneObject):
                                      float(self.__centerPos[1] + y), 
                                      float(self.__centerPos[2] + z)])
         return vertices
+    
+    def __genWireframe(self, vertices):
+        indices = [0, 1, 1, 3, 3, 2, 2, 0,
+                   2, 6, 6, 4, 4, 0,
+                   6, 7, 7, 5, 5, 4,
+                   7, 3, 1, 5]
+        color = [0, 0, 0]
+        self.addChildObject(Wireframe(vertices, indices, color))
