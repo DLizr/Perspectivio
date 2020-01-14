0d010436addc454e984ba63edf5a3d66490f9646
DLizr <Dmitrylizr@yandex.ru>
2020-01-14 11:55:59 +0300

    Fixed memory leak, level selection screen.

--- src/process/MainMenu.py
+++ src/process/MainMenu.py
@@ -19,7 +19,7 @@ class MainMenu(Process):
         self.__size = (width, height)
         self.__eventHandler = EventHandler()
         self.__eventHandler.setMouseHandler(MainMenuMouseHandler(self))
-        self.addButton(0, 0, "PlayButton", 0)
+        self.addButton(289, 250, "PlayButton", self.__openLevelSelectionScreen)
 
     def update(self):
         self.__eventHandler.handleEvents()
@@ -37,12 +37,18 @@ class MainMenu(Process):
         width, height = imgIdle.get_size()
 
         button = ButtonRenderer(name, (x, y), imgIdle, imgHover, imgPress)
-        self.__eventHandler.getMouseHandler().addButton(x, y, x + width, y + height, name, button.idle, self.__openLevelSelectionScreen, button.hover)
+        self.__eventHandler.getMouseHandler().addButton(x, y, x + width, y + height, name, button.idle, action, button.hover)
         self.__buttons.add(button)
     
     def __openLevelSelectionScreen(self):
-        for i in self.__buttons:
-            if i.getName() == "PlayButton":
-                i.click()
-                return
-        print("Yay")
+        self.__buttons.clear()
+        self.__eventHandler.getMouseHandler().clearButtons()
+
+        self.addButton(50, 50, "1", lambda: self.__startLevel(1))
+        self.addButton(200, 50, "2", lambda: self.__startLevel(2))
+        self.addButton(350, 50, "3", lambda: self.__startLevel(3))
+        self.addButton(500, 50, "4", lambda: self.__startLevel(4))
+        self.addButton(650, 50, "5", lambda: self.__startLevel(5))
+
+    def __startLevel(self, level: int):
+        print("Level {} has been loaded".format(level))
